40bc07dae7c4c306f19e9304cbc0e09ab67967d0
DiegoRaC14 <141955933+DiegoRaC14@users.noreply.github.com>
2023-10-05 09:53:24 -0600

    Add files via upload

---
+++ Examen 2 ejercicio 6 - Corrección de programa.cpp
@@ -0,0 +1,29 @@
+/* Corrección de programa que dice si la suma de 2 números que ingresamos es correcta o no*/
+
+#include <stdio.h>
+
+int main (){
+	
+	int n1, n2, resultado, suma;
+	
+	printf("Introduzca un numero:\n");
+	scanf("%d", &n1);
+	
+	printf("Introduzca otro numero:\n");
+	scanf("%d",&n2);
+	
+	printf("Cuanto suman?:\n");
+	scanf("%d",&suma);
+	
+	resultado=n1+n2; //Operación
+	
+	if (suma==resultado)
+	{
+		printf("Correcto\n");
+	}
+	else{
+		printf("INCORRECTO: La suma es %d.\n",resultado);
+	}
+	
+	return 0;
+}
